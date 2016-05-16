b0200eb7ff6089f21c1e9359e34926e6a0d9f12f
Anders Elo <anders@kaerus.com>
2016-05-16 20:27:32 +0200

    min file

--- asearch.js
+++ asearch.js
@@ -1,7 +1,7 @@
 /*
  * pandaCommerce auto complete jQuery plugin
  * Author <anders at restfuldesign com>
- * Version 0.0.4
+ * Version 0.0.5
  */
 
 /*global location jQuery setTimeout clearTimeout */
